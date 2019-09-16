f5bde7c3c4a6cdbd012f1bfa63e88efc9ce53d5c
Francesc Alted <faltet@gmail.com>
2019-09-16 12:35:17 +0200

    Getting ready for release 0.2.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ Topic :: Software Development :: Testing
 from setuptools import setup, find_packages
 setup(
     name="ipython_memwatcher",
-    version="0.2.1",
+    version="0.2.3",
     url="https://github.com/FrancescAlted/ipython_memwatcher",
     author="Francesc Alted, Ian Ozsvald",
     author_email="faltet AT gmail DOT com",
@@ -39,5 +39,5 @@ setup(
     platforms=["Any."],
     packages=['ipython_memwatcher'],
     #package_dir={'ipython_memwatcher': 'ipython_memwatcher'},
-    install_requires=['IPython>=2.1', 'memory_profiler']
+    install_requires=['IPython>=3.1', 'memory_profiler']
 )
